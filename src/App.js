db7db1ccef944557841c27cb85ed0fb2882d6cc7
pradeep <pradeep.9997@gmail.com>
2024-02-19 22:13:53 +0530

    api Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  let [products,setProducts]=useState([])
+  let getProduct=()=>{
+    axios.get('https://dummyjson.com/products')
+    .then((Res)=>Res.data)
+    .then((finalRes)=>{
+      console.log(finalRes.products)
+      setProducts(finalRes.products)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+ 
+
+ 
+
+  useEffect(()=>{
+    getProduct()
+    
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <h1 className='text-[40px] font-bold py-[48px]'>Our Products</h1>
+      <div className='max-w-[1320px] mx-auto grid grid-cols-4 gap-[20px]'>
+        {
+           products.length>1 ?
+           products.map((items,index)=>{
+            return(
+              <ProductCard items={items} key={index}/>
+            )
+           })
+          
+           :
+           <div>Loading......</div>
+        }
+       
+       
+       
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function ProductCard({items}){
+  return(
+    <div className='shadow-lg'>
+      <img className='w-[100%] h-[250px]' src={items.thumbnail}/>
+      <h3 className='text-[20px] text-center py-[15px] bg-red-900 text-white'>
+        {items.title}
+      </h3>
+  </div>
+  )
+}
